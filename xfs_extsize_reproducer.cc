13478f40b6cda282349d8f4190d21afb6e7c9d4f
Samuel Just <sam.just@inktank.com>
2014-07-14 16:43:56 -0700

    worknig

--- xfs_extsize_reproducer.cc
+++ xfs_extsize_reproducer.cc
@@ -8,6 +8,8 @@
 #include <iostream>
 #include <string.h>
 
+#define OBJSIZE (4<<20)
+
 int main() {
   int randfd = open("/dev/urandom", O_RDONLY);
   int fd = open("test", O_RDWR|O_CREAT|O_EXCL, 0666);
@@ -51,18 +53,18 @@ int main() {
     std::cerr << "successfully set to " << fsx2.fsx_extsize << std::endl;
   }
 
-  char buf[4<<20];
-  char check[4<<20];
-  memset(check, 0, sizeof(check));
-  memset(buf, 0, sizeof(buf));
+  char *buf = new char[OBJSIZE];
+  char *check = new char[OBJSIZE];
+  memset(check, 0, OBJSIZE);
+  memset(buf, 0, OBJSIZE);
   int offset = -1;
   int len = -1;
   while (scanf("%d %d\n", &offset, &len) == 2) {
     assert(len >= 0);
     assert(offset >= 0);
     std::cout << offset << "~" << len << std::endl;
-    assert(len < sizeof(buf));
-    assert(offset + len < sizeof(check));
+    assert(len < OBJSIZE);
+    assert(offset + len < OBJSIZE);
 
     r = read(randfd, buf, len);
     assert(r == len);
@@ -77,10 +79,13 @@ int main() {
     assert(fd >= 0);
   }
 
-  r = pread(fd, buf, sizeof(buf), 0);
-  assert(r == sizeof(buf));
-  r = memcmp(buf, check, sizeof(buf));
+  r = pread(fd, buf, OBJSIZE, 0);
+  assert(r == OBJSIZE);
+  r = memcmp(buf, check, OBJSIZE);
   assert(r == 0);
+
+  delete[] buf;
+  delete[] check;
   close(fd);
   close(randfd);
 }
