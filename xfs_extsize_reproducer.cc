2257374179f254c6763c3c5b9572ccc3fe4e4cd2
Samuel Just <sam.just@inktank.com>
2014-07-15 10:44:24 -0700

    worknig

--- xfs_extsize_reproducer.cc
+++ xfs_extsize_reproducer.cc
@@ -13,47 +13,51 @@
 #define OBJSIZE (4<<20)
 #define ATTRSIZEMAX (1<<10)
 
-int main() {
+int main(int argc, char **argv) {
   int fd = open("test", O_RDWR|O_CREAT|O_EXCL, 0666);
   assert(fd >= 0);
   int r = 0;
 
-  struct fsxattr fsx;
-  r = ioctl(fd, XFS_IOC_FSGETXATTR, &fsx);
-  if (r < 0) {
-    int ret = -errno;
-    std::cerr << "FSGETXATTR: " << ret << std::endl;
-    return ret;
-  }
+  if (argc < 2 || argv[1][0] == 'e') {
+    struct fsxattr fsx;
+    r = ioctl(fd, XFS_IOC_FSGETXATTR, &fsx);
+    if (r < 0) {
+      int ret = -errno;
+      std::cerr << "FSGETXATTR: " << ret << std::endl;
+      return ret;
+    }
 
-  // already set?
-  if (fsx.fsx_xflags & XFS_XFLAG_EXTSIZE) {
-    std::cerr << "already set" << std::endl;
-    return 0;
-  }
+    // already set?
+    if (fsx.fsx_xflags & XFS_XFLAG_EXTSIZE) {
+      std::cerr << "already set" << std::endl;
+      return 0;
+    }
 
-  std::cerr << fsx.fsx_nextents << " exents, extsize is " << fsx.fsx_extsize << std::endl;
+    std::cerr << fsx.fsx_nextents << " exents, extsize is " << fsx.fsx_extsize << std::endl;
 
-  unsigned val = 4<<20;
-  fsx.fsx_xflags |= XFS_XFLAG_EXTSIZE;
-  fsx.fsx_extsize = val;
+    unsigned val = 4<<20;
+    fsx.fsx_xflags |= XFS_XFLAG_EXTSIZE;
+    fsx.fsx_extsize = val;
   
-  if (ioctl(fd, XFS_IOC_FSSETXATTR, &fsx) < 0) {
-    int ret = -errno;
-    std::cerr << "FSSETXATTR: " << ret << std::endl;
-    return ret;
-  }
+    if (ioctl(fd, XFS_IOC_FSSETXATTR, &fsx) < 0) {
+      int ret = -errno;
+      std::cerr << "FSSETXATTR: " << ret << std::endl;
+      return ret;
+    }
 
-  struct fsxattr fsx2;
-  r = ioctl(fd, XFS_IOC_FSGETXATTR, &fsx2);
-  if (r < 0) {
-    int ret = -errno;
-    std::cerr << "FSGETXATTR: " << ret << std::endl;
-    return ret;
-  }
+    struct fsxattr fsx2;
+    r = ioctl(fd, XFS_IOC_FSGETXATTR, &fsx2);
+    if (r < 0) {
+      int ret = -errno;
+      std::cerr << "FSGETXATTR: " << ret << std::endl;
+      return ret;
+    }
 
-  if (fsx2.fsx_xflags & XFS_XFLAG_EXTSIZE) {
-    std::cerr << "successfully set to " << fsx2.fsx_extsize << std::endl;
+    if (fsx2.fsx_xflags & XFS_XFLAG_EXTSIZE) {
+      std::cerr << "successfully set to " << fsx2.fsx_extsize << std::endl;
+    }
+  } else {
+    std::cerr << "did not set extsize" << std::endl;
   }
 
   char *buf = new char[OBJSIZE];
