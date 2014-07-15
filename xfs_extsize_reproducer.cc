51cc5a9651f34e947f50682c112107187c033af8
Samuel Just <sam.just@inktank.com>
2014-07-15 10:22:41 -0700

    worknig

--- xfs_extsize_reproducer.cc
+++ xfs_extsize_reproducer.cc
@@ -85,9 +85,12 @@ int main() {
     assert(len <= OBJSIZE);
     assert(offset + len <= OBJSIZE);
 
+#if 0
     if (len > 8<<10) {
       memset(buf, 0, len);
     } else {
+#endif
+      {
       r = read(randfd, buf, len);
       assert(r == len);
     }
